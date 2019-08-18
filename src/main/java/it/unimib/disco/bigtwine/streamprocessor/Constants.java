c4c7fa585a4baf755a384e13afc44baf6c925465
Fausto <fausto91@gmail.com>
2019-08-18 09:51:14 +0200

    Possibilità di override costanti con variabili d'ambiente

--- src/main/java/it/unimib/disco/bigtwine/streamprocessor/Constants.java
+++ src/main/java/it/unimib/disco/bigtwine/streamprocessor/Constants.java
@@ -1,21 +1,31 @@
 package it.unimib.disco.bigtwine.streamprocessor;
 
 public class Constants {
-    public static final String KAFKA_BOOTSTRAP_SERVERS = "kafka:9092";
+    public static final String KAFKA_BOOTSTRAP_SERVERS = getEnv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092");
 
-    public static final String GRIDFS_HOST = "mysql";
-    public static final int GRIDFS_PORT = 27017;
-    public static final String GRIDFS_DB = "analysis";
+    public static final String GRIDFS_HOST = getEnv("GRIDFS_HOST", "mongodb");
+    public static final int GRIDFS_PORT = getEnv("GRIDFS_PORT", 27017);
+    public static final String GRIDFS_DB = getEnv("GRIDFS_DB", "analysis");
 
-    public static final String NER_INPUT_TOPIC = "ner-requests";
-    public static final String NER_OUTPUT_TOPIC = "ner-responses.%s";
+    public static final String NER_INPUT_TOPIC = getEnv("NER_INPUT_TOPIC", "ner-requests");
+    public static final String NER_OUTPUT_TOPIC = getEnv("NER_OUTPUT_TOPIC", "ner-responses.%s");
 
-    public static final String NEL_INPUT_TOPIC = "nel-requests";
-    public static final String NEL_OUTPUT_TOPIC = "nel-responses.%s";
+    public static final String NEL_INPUT_TOPIC = getEnv("NEL_INPUT_TOPIC", "nel-requests");
+    public static final String NEL_OUTPUT_TOPIC = getEnv("NEL_OUTPUT_TOPIC", "nel-responses.%s");
 
-    public static final String LINKRESOLVER_INPUT_TOPIC = "linkresolver-requests";
-    public static final String LINKRESOLVER_OUTPUT_TOPIC = "linkresolver-responses.%s";
+    public static final String LINKRESOLVER_INPUT_TOPIC = getEnv("LINKRESOLVER_INPUT_TOPIC", "linkresolver-requests");
+    public static final String LINKRESOLVER_OUTPUT_TOPIC = getEnv("LINKRESOLVER_OUTPUT_TOPIC", "linkresolver-responses.%s");
 
-    public static final String GEODECODER_INPUT_TOPIC = "geodecoder-requests";
-    public static final String GEODECODER_OUTPUT_TOPIC = "geodecoder-responses.%s";
+    public static final String GEODECODER_INPUT_TOPIC = getEnv("GEODECODER_INPUT_TOPIC", "geodecoder-requests");
+    public static final String GEODECODER_OUTPUT_TOPIC = getEnv("GEODECODER_OUTPUT_TOPIC", "geodecoder-responses.%s");
+
+    public static String getEnv(String name, String fallback) {
+        String value = System.getenv(name);
+        return value != null ? value : fallback;
+    }
+
+    public static int getEnv(String name, int fallback) {
+        String value = System.getenv(name);
+        return value != null ? Integer.valueOf(value) : fallback;
+    }
 }
